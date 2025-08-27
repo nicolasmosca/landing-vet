b04803e390321003237fcd0b1d1b4357a3b809f1
root <root@e5aa34ac19bd>
2025-08-27 17:37:22 +0000

    feat: changes by Codlylabs AI agent

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import VetShopLandingPage from './components/VetShopLandingPage';
+
+function App() {
+  return (
+    <div className="App">
+      <VetShopLandingPage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './App.css';
+import VetShopLandingPage from './components/VetShopLandingPage';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <VetShopLandingPage />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
